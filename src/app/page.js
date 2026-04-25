4a180fc74f2671ec3434602a20893ae42eb6f238
Yash <proyash1611@gmail.com>
2026-04-25 16:04:20 +0530

    Fix missing useEffect import in page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 'use client';
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useEffect } from 'react';
 import Link from 'next/link';
 import AcademyCard from '../components/AcademyCard';
 import SearchBar from '../components/SearchBar';
